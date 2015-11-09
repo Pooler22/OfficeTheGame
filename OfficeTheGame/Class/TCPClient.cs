89a07326df8a2b480ecc1ba844a24fae14489b32
pooler <rudypawel12@gmail.com>
2015-11-09 14:55:35 +0100

    move to windows 10

---
+++ OfficeTheGame/Class/TCPClient.cs
@@ -0,0 +1,22 @@
+﻿namespace OfficeTheGame.Class
+{
+    internal abstract class TCPClient
+    {
+        public string name;
+        private PlayPage playPage;
+
+        public void initTCPClient(PlayPage playpage, string name)
+        {
+            this.playPage = playpage;
+            this.name = name;
+        }
+
+        public abstract void initListener(string portListener);
+
+        public abstract void initSender(string portSender, string remoteAdress);
+
+        public abstract void SendRequest(string request);
+
+        public abstract void Dispose();
+    }
+}
